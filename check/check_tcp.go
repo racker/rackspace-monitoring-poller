799d0c0b9425b5e58c5fcd2e58edb1fc264108a9
Ryan Phillips <ryan.phillips@rackspace.com>
2016-11-09 16:05:28 -0600

    add initial support for tcp check

--- check/check_tcp.go
+++ check/check_tcp.go
@@ -1,20 +1,76 @@
 package check
 
 import (
-	"errors"
+	"crypto/tls"
+	"encoding/json"
 	log "github.com/Sirupsen/logrus"
+	"io"
+	"net"
+	"strconv"
+	"time"
 )
 
 type TCPCheck struct {
 	CheckBase
+	Details struct {
+		BannerMatch string `json:"banner_match"`
+		BodyMatch   string `json:"body_match"`
+		Port        uint64 `json:"port"`
+		SendBody    string `json:"send_body"`
+		UseSSL      bool   `json:"ssl"`
+	}
 }
 
 func NewTCPCheck(base *CheckBase) Check {
 	check := &TCPCheck{CheckBase: *base}
+	err := json.Unmarshal(*base.Details, &check.Details)
+	if err != nil {
+		log.Error("Error unmarshalling TCPCheck")
+		return nil
+	}
+	check.PrintDefaults()
 	return check
 }
 
+func (ch *TCPCheck) GenerateAddress() (string, error) {
+	portStr := strconv.FormatUint(ch.Details.Port, 10)
+	ip, err := ch.GetTargetIP()
+	if err != nil {
+		return "", err
+	}
+	return net.JoinHostPort(ip, portStr), nil
+}
+
 func (ch *TCPCheck) Run() (*CheckResultSet, error) {
-	log.Printf("Running TCP Check: %v\n", ch.GetId())
-	return nil, errors.New("Not implimented")
+	var conn net.Conn
+	var err error
+	starttime := NowTimestampMillis()
+	addr, _ := ch.GenerateAddress()
+	nd := &net.Dialer{Timeout: time.Duration(ch.GetTimeout()) * time.Second}
+	log.WithFields(log.Fields{
+		"type":    ch.CheckType,
+		"id":      ch.Id,
+		"address": addr,
+		"ssl":     ch.Details.UseSSL,
+	}).Info("Running TCP Check")
+	if ch.Details.UseSSL {
+		TLSconfig := &tls.Config{}
+		conn, err = tls.DialWithDialer(nd, "tcp", addr, TLSconfig)
+	} else {
+		conn, err = nd.Dial("tcp", addr)
+	}
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	defer conn.Close()
+	conn.SetDeadline(time.Now().Add(time.Duration(ch.GetTimeout()) * time.Second))
+	if len(ch.Details.SendBody) > 0 {
+		io.WriteString(conn, ch.Details.SendBody)
+	}
+	endtime := NowTimestampMillis()
+	cr := NewCheckResult(
+		NewMetric("duration", "", MetricNumber, endtime-starttime, "ms"),
+	)
+	return NewCheckResultSet(ch, cr), nil
 }
