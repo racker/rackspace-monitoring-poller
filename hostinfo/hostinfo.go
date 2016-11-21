d860a3011f8c8724a40906dbb48e1dbc9bb47f03
Geoff Bourne <geoff.bourne@rackspace.com>
2016-11-21 12:46:36 -0600

    Refactor more into protocol package to enable server-side code leverage * also cascaded to refactoring config to its own package

--- hostinfo/hostinfo.go
+++ hostinfo/hostinfo.go
@@ -34,3 +34,11 @@ func NewHostInfoResponse(cr *check.CheckResult, f *protocol.FrameMsg, hinfo Host
 
 	return resp
 }
+
+func NewHostInfoResponse(cr *check.CheckResult, f *protocol.FrameMsg, hinfo HostInfo) *protocol.HostInfoResponse {
+	resp := &protocol.HostInfoResponse{}
+	resp.Result = hinfo.BuildResult(cr)
+	resp.SetResponseFrameMsg(f)
+
+	return resp
+}
