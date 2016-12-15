b5efa425298e3458230c8337445b00735c1ea5da
Ryan Phillips <ryan@trolocsis.com>
2016-12-15 12:36:34 -0600

    add http success test (#51)
    * add http success test
    
    * add more tests

---
+++ check/check_http_test.go
@@ -0,0 +1,146 @@
+//
+// Copyright 2016 Rackspace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package check_test
+
+import (
+	"context"
+	"fmt"
+	check "github.com/racker/rackspace-monitoring-poller/check"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHTTPSuccess(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chPzAHTTP",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"url":"%s"},
+	  "type":"remote.http",
+	  "timeout":15,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":"",
+	  "disabled":false
+	  }`, ts.URL)
+	check := check.NewCheck([]byte(checkData))
+
+	// Run check
+	crs, err := check.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Validate Metrics
+	if crs.Available == false {
+		t.Fatal("availability should be true")
+	}
+
+	metrics := []string{
+		"bytes",
+		"code",
+
+		"truncated",
+		"tt_connect",
+		"tt_firstbyte",
+	}
+	ValidateMetrics(t, metrics, crs.Get(0))
+}
+
+func TestHTTPClosed(t *testing.T) {
+	// Create a server then close it
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	ts.Close()
+
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chPzAHTTP",
+	  "zone_id":"pzA",
+
+	  "details":{"url":"%s"},
+	  "type":"remote.http",
+	  "timeout":15,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":"",
+	  "disabled":false
+	  }`, ts.URL)
+	check := check.NewCheck([]byte(checkData))
+
+	// Run check
+	crs, err := check.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Validate Metrics
+	if crs.Available == true {
+		t.Fatal("availability should be false")
+	}
+}
+
+func TestHTTPTimeout(t *testing.T) {
+	// Create a cancellable context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Create a server that times out
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		<-ctx.Done()
+	}))
+	defer ts.Close()
+
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chPzAHTTPTimeout",
+	  "zone_id":"pzA",
+	  "details":{"url":"%s"},
+	  "type":"remote.http",
+	  "timeout":1,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":"",
+	  "disabled":false
+	  }`, ts.URL)
+	check := check.NewCheck([]byte(checkData))
+
+	// Run check
+	crs, err := check.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Validate Metrics
+	if crs.Available == true {
+		t.Fatal("availability should be false")
+	}
+
+	cancel()
+}
