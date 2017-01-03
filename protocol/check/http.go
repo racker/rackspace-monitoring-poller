715fc46ec9d821b456639a1e054a0e176f863531
Ryan Phillips <ryan@trolocsis.com>
2017-01-03 09:12:45 -0600

    add body match and body_matches functionality and tests (#55)
    * add body validation test
    
    * add a header check
    
    * fix truncated
    
    * Should be bytes
    
    * http: body match functionality

--- protocol/check/http.go
+++ protocol/check/http.go
@@ -18,15 +18,15 @@ package check
 
 type HTTPCheckDetails struct {
 	Details struct {
-		AuthPassword    string              `json:"auth_password"`
-		AuthUser        string              `json:"auth_user"`
-		Body            string              `json:"body"`
-		BodyMatches     []map[string]string `json:"body_matches"`
-		FollowRedirects bool                `json:"follow_redirects"`
-		Headers         map[string]string   `json:"headers"`
-		IncludeBody     bool                `json:"include_body"`
-		Method          string              `json:"method"`
-		Url             string              `json:"url"`
+		AuthPassword    string            `json:"auth_password"`
+		AuthUser        string            `json:"auth_user"`
+		Body            string            `json:"body"`
+		BodyMatches     map[string]string `json:"body_matches"`
+		FollowRedirects bool              `json:"follow_redirects"`
+		Headers         map[string]string `json:"headers"`
+		IncludeBody     bool              `json:"include_body"`
+		Method          string            `json:"method"`
+		Url             string            `json:"url"`
 	}
 }
 
