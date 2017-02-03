cdd15b4a4466f032286656447a5c46648b38a9e9
Ryan Phillips <ryan.phillips@rackspace.com>
2017-02-03 11:01:26 -0600

    add a version command
    * Sets the version from the built tag

--- poller.go
+++ poller.go
@@ -22,6 +22,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"os"
 	"time"
@@ -47,7 +48,17 @@ var (
 	Formatter log.Formatter = &log.JSONFormatter{
 		TimestampFormat: time.RFC1123,
 	}
+
+	Version = "1.0.0+git"
 )
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(Version)
+	},
+}
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -67,6 +78,7 @@ func initEnv() {
 }
 
 func main() {
+	pollerCmd.AddCommand(versionCmd)
 	pollerCmd.AddCommand(commands.ServeCmd)
 	pollerCmd.AddCommand(commands.EndpointCmd)
 	pollerCmd.Execute()
