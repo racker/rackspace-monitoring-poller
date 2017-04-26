996ccdc65f053909448e41ef63b102612f4d7930
Ryan Phillips <ryan@trolocsis.com>
2017-04-26 08:25:24 -0500

    add check plugin (#152)
    * add check plugin
    
    * change to MustCompile regexps
    
    * case optimizations
    
    * skip closeport test
    
    * add log lines for metrics
    
    * change error to errored
    
    * more tests
    
    * globalize regexp's
    
    * cleanup

---
+++ check/check_plugin.go
@@ -0,0 +1,209 @@
+// Copyright 2017 Rackspace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package check
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/racker/rackspace-monitoring-poller/protocol/metric"
+
+	log "github.com/Sirupsen/logrus"
+	protocol "github.com/racker/rackspace-monitoring-poller/protocol/check"
+)
+
+const (
+	ErrorPluginExit = "Plugin exited with non-zero status code"
+)
+
+var (
+	statusRegex = regexp.MustCompile("^status\\s+(err|warn|ok)\\s+(.*)")
+	stateRegex  = regexp.MustCompile("^state\\s+(.*?)")
+	metricRegex = regexp.MustCompile("^metric\\s+(.*?)\\s+(.*?)\\s+(.*)")
+)
+
+type PluginCheck struct {
+	Base
+	protocol.PluginCheckDetails
+}
+
+func NewPluginCheck(base *Base) (Check, error) {
+	check := &PluginCheck{Base: *base}
+	err := json.Unmarshal(*base.RawDetails, &check.Details)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"prefix":  "check_plugin",
+			"err":     err,
+			"details": string(*base.RawDetails),
+		}).Error("Unable to unmarshal check details")
+		return nil, err
+	}
+	return check, nil
+}
+
+func (ch *PluginCheck) handleStdout(stdout io.Reader, stdoutReadDone chan struct{}, crs *ResultSet) {
+	defer close(stdoutReadDone)
+	scanner := bufio.NewScanner(stdout)
+	cr := crs.Get(0)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		log.WithFields(log.Fields{
+			"prefix": ch.GetLogPrefix() + ":stdout",
+			"id":     ch.Id,
+			"line":   line,
+		}).Debug("output")
+		if matches := statusRegex.FindStringSubmatch(line); matches != nil {
+			switch strings.ToLower(matches[1]) {
+			case "ok", "warn", "err":
+				crs.SetStatus(matches[2])
+			default:
+				crs.SetStatus(strings.Join(matches[1:], " "))
+			}
+		}
+		if matches := stateRegex.FindStringSubmatch(line); matches != nil {
+			fields := strings.Fields(line)
+			if len(fields) > 1 {
+				state := strings.ToLower(fields[1])
+				switch state {
+				case "available":
+					crs.SetStateAvailable()
+				case "unavailable":
+					crs.SetStateUnavailable()
+				}
+			}
+		}
+		if matches := metricRegex.FindStringSubmatch(line); matches != nil {
+			metricName := matches[1]
+			metricUnit := strings.ToLower(matches[2])
+			metricValue := matches[3]
+			var pollerType int
+			switch metricUnit {
+			case "string":
+				pollerType = metric.MetricString
+			case "double", "float":
+				pollerType = metric.MetricFloat
+			case "gauge", "int", "int32", "uint32", "int64", "uint64":
+				pollerType = metric.MetricNumber
+			default:
+				continue
+			}
+			log.WithFields(log.Fields{
+				"prefix": ch.GetLogPrefix(),
+				"id":     ch.Id,
+				"unit":   metricUnit,
+				"value":  metricValue,
+			}).Debug("Add metric")
+			cr.AddMetric(metric.NewMetric(metricName, "", pollerType, metricValue, metricUnit))
+		}
+	}
+}
+
+func (ch *PluginCheck) setupEnvironment(cmd *exec.Cmd) {
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, fmt.Sprintf("RAX_CHECK_ID=%v", ch.Id))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("RAX_CHECK_PERIOD=%v", ch.Period))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("RAX_CHECK_TYPE=%v", ch.GetCheckType()))
+}
+
+func (ch *PluginCheck) Run() (*ResultSet, error) {
+	// Setup timeout
+	timeout := uint64(ch.Details.Timeout)
+	if timeout == 0 {
+		timeout = ch.Timeout
+	}
+	ctxTimeout := time.Duration(timeout) * time.Second
+
+	log.WithFields(log.Fields{
+		"prefix":  ch.GetLogPrefix(),
+		"args":    ch.Details.Args,
+		"file":    ch.Details.File,
+		"id":      ch.Id,
+		"timeout": ctxTimeout,
+	}).Debug("Running Plugin Check")
+
+	// Set Context
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+
+	// Setup results
+	cr := NewResult()
+	crs := NewResultSet(ch, cr)
+	crs.SetStateAvailable()
+
+	// Setup stdin pipe, which gets closed
+	r, _, _ := os.Pipe()
+
+	// Command Setup
+	cmd := exec.CommandContext(ctx, ch.Details.File, ch.Details.Args...)
+	ch.setupEnvironment(cmd)
+
+	// Set I/O
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		crs.SetStateUnavailable()
+		crs.SetStatus(err.Error())
+		return crs, nil
+	}
+	cmd.Stdin = r
+
+	// Start process
+	if err := cmd.Start(); err != nil {
+		log.WithFields(log.Fields{
+			"prefix": ch.GetLogPrefix(),
+			"id":     ch.Id,
+		}).Debug("Plugin start failed")
+		r.Close()
+		crs.SetStateUnavailable()
+		crs.SetStatus(ErrorPluginExit)
+		return crs, nil
+	}
+	// Close stdin
+	r.Close()
+
+	stdoutReadDone := make(chan struct{})
+	go ch.handleStdout(stdout, stdoutReadDone, crs)
+
+	// Wait for commmand to finish
+	var errorFlag bool
+	if err := cmd.Wait(); err != nil {
+		log.WithFields(log.Fields{
+			"prefix": ch.GetLogPrefix(),
+			"id":     ch.Id,
+			"error":  err.Error(),
+		}).Debug("Plugin wait failed")
+		crs.SetStateUnavailable()
+		crs.SetStatus(ErrorPluginExit)
+		errorFlag = true
+	}
+
+	// drain the stdout done channel
+	<-stdoutReadDone
+
+	log.WithFields(log.Fields{
+		"prefix":  ch.GetLogPrefix(),
+		"id":      ch.Id,
+		"errored": errorFlag,
+	}).Debug("End Plugin Check")
+
+	return crs, nil
+}
---
+++ check/check_plugin_test.go
@@ -0,0 +1,372 @@
+// Copyright 2017 Rackspace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package check_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/racker/rackspace-monitoring-poller/check"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAgentPlugin_RunSuccess(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":15,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/cloudkick_agent_custom_plugin_1.sh")
+	check, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := check.Run()
+	require.NoError(t, err)
+	require.True(t, crs.Available, "validate errored")
+
+	cr := crs.Get(0)
+	require.NotNil(t, cr.GetMetric("logged_users"), "validate logged_users metric")
+	require.NotNil(t, cr.GetMetric("active_processes"), "validate logged_users metric")
+}
+
+func TestAgentPlugin_RunTimeout(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":1,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/cloudkick_agent_custom_plugin_timeout.sh")
+	check, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := check.Run()
+	require.NoError(t, err)
+
+	require.False(t, crs.Available, "validate errored")
+}
+
+func TestAgentPlugin_WarnPlugin2(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/cloudkick_agent_custom_plugin_2.sh")
+	check, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := check.Run()
+	require.NoError(t, err)
+
+	require.True(t, crs.Available, "status is warn")
+}
+
+func TestAgentPlugin_InvalidMetricLines1(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/invalid_metric_lines_1.sh")
+	check, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := check.Run()
+	require.NoError(t, err)
+
+	require.True(t, crs.Available, "status is invalid")
+	cr := crs.Get(0)
+	require.Nil(t, cr.GetMetric("metric1"), "validate metric1 metric")
+	require.Nil(t, cr.GetMetric("foo"), "validate foo metric")
+	require.NotNil(t, cr.GetMetric("metric8"), "validate metric8 metric")
+	require.NotNil(t, cr.GetMetric("metric7"), "validate metric7 metric")
+	require.NotNil(t, cr.GetMetric("metric2"), "validate metric2 metric")
+}
+
+func TestAgentPlugin_InvalidMetricLines2(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/invalid_metric_lines_2.sh")
+	check, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := check.Run()
+	require.NoError(t, err)
+
+	require.True(t, crs.Available, "status is invalid")
+	cr := crs.Get(0)
+	require.Nil(t, cr.GetMetric("foo"), "validate foo metric")
+	require.Nil(t, cr.GetMetric("metric1"), "validate metric1 metric")
+	require.NotNil(t, cr.GetMetric("metric7"), "validate metric7 metric")
+	require.NotNil(t, cr.GetMetric("metric8"), "validate metric8 metric")
+	require.NotNil(t, cr.GetMetric("metric2"), "validate metric1 metric")
+	require.NotNil(t, cr.GetMetric("metric2"), "validate metric2 metric")
+}
+
+func TestAgentPlugin_InvalidMetricLines3(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/invalid_metric_lines_3.sh")
+	check, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := check.Run()
+	require.NoError(t, err)
+
+	require.True(t, crs.Available, "status is invalid")
+	cr := crs.Get(0)
+	require.Nil(t, cr.GetMetric("foo"), "validate foo metric")
+	require.Nil(t, cr.GetMetric("metric1"), "validate metric1 metric")
+	require.NotNil(t, cr.GetMetric("metric8"), "validate metric8 metric")
+	require.NotNil(t, cr.GetMetric("metric7"), "validate metric7 metric")
+	require.NotNil(t, cr.GetMetric("metric2"), "validate metric1 metric")
+	require.NotNil(t, cr.GetMetric("metric2"), "validate metric2 metric")
+}
+
+func TestAgentPlugin_InvalidMetricLines4(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/invalid_metric_lines_4.sh")
+	check, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := check.Run()
+	require.NoError(t, err)
+
+	require.True(t, crs.Available, "status is invalid")
+	cr := crs.Get(0)
+	require.Nil(t, cr.GetMetric("foo"), "validate foo metric")
+	require.Nil(t, cr.GetMetric("metric1"), "validate metric1 metric")
+	require.NotNil(t, cr.GetMetric("metric8"), "validate metric8 metric")
+	require.NotNil(t, cr.GetMetric("metric7"), "validate metric7 metric")
+	require.NotNil(t, cr.GetMetric("metric2"), "validate metric1 metric")
+	require.NotNil(t, cr.GetMetric("metric2"), "validate metric2 metric")
+}
+
+func TestAgentPlugin_StatusCodeNonZero(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/non_zero_with_status.sh")
+	ch, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := ch.Run()
+	require.NoError(t, err)
+
+	require.False(t, crs.Available, "status is invalid")
+	require.Equal(t, crs.Status, check.ErrorPluginExit)
+}
+
+func TestAgentPlugin_NotExecutable(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/not_executable.sh")
+	ch, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := ch.Run()
+	require.NoError(t, err)
+
+	require.False(t, crs.Available, "status is invalid")
+	require.Equal(t, crs.Status, check.ErrorPluginExit)
+}
+
+func TestAgentPlugin_PartialOutputWithSleep(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/partial_output_with_sleep.sh")
+	ch, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := ch.Run()
+	require.NoError(t, err)
+
+	cr := crs.Get(0)
+	require.True(t, crs.Available)
+	require.NotNil(t, cr.GetMetric("logged_users"))
+	require.NotNil(t, cr.GetMetric("active_processes"))
+	require.NotNil(t, cr.GetMetric("avg_wait_time"))
+	require.NotNil(t, cr.GetMetric("something"))
+	require.NotNil(t, cr.GetMetric("packet_count"))
+}
+
+func TestAgentPlugin_Plugin1(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+	  "details":{"file":"%s"},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/plugin_1.sh")
+	ch, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := ch.Run()
+	require.NoError(t, err)
+
+	cr := crs.Get(0)
+	require.True(t, crs.Available)
+	require.NotNil(t, cr.GetMetric("logged_users"))
+	require.NotNil(t, cr.GetMetric("active_processes"))
+	require.NotNil(t, cr.GetMetric("avg_wait_time"))
+	require.NotNil(t, cr.GetMetric("something"))
+	require.NotNil(t, cr.GetMetric("packet_count"))
+}
+
+func TestAgentPlugin_PluginCustomArguments(t *testing.T) {
+	// Create Check
+	checkData := fmt.Sprintf(`{
+	  "id":"chTestPlugin",
+	  "zone_id":"pzA",
+	  "entity_id":"enAAAAIPV4",
+		"details":{"file":"%s", "args": ["a", "b"]},
+	  "type":"agent.plugin",
+	  "timeout":5,
+	  "period":30,
+	  "ip_addresses":{"default":"127.0.0.1"},
+	  "target_alias":"default",
+	  "target_hostname":"",
+	  "target_resolver":1,
+	  "disabled":false
+	  }`, "fixtures/plugin_custom_arguments.sh")
+	ch, err := check.NewCheck(context.Background(), []byte(checkData))
+	require.NoError(t, err)
+
+	crs, err := ch.Run()
+	require.NoError(t, err)
+
+	cr := crs.Get(0)
+	require.True(t, crs.Available)
+	require.NotNil(t, cr.GetMetric("a"))
+	require.NotNil(t, cr.GetMetric("b"))
+	require.Nil(t, cr.GetMetric("c"))
+}
