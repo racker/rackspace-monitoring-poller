613a79e9a4e42e226b83c7346cd01ab7d9b9682d
Geoff Bourne <geoff.bourne@rackspace.com>
2017-10-11 16:17:54 -0500

    Revert "Build separate DEBs for upstart and systemd uses"
    This reverts commit 6061efe

--- contrib/vms/provision.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc | sudo apt-key add -
-
-echo "deb [arch=amd64] http://stable.poller.packages.cloudmonitoring.rackspace.com/debian cloudmonitoring main" > /etc/apt/sources.list.d/cloudmonitoring.list
-
-apt-get update
-
