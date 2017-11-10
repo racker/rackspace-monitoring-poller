4c4b2c2b206670d9e36404f191823f59dbf7453d
Geoff Bourne <geoff.bourne@rackspace.com>
2017-11-10 11:42:15 -0600

    Revert "See if running `go test` in sudo fixes ping tests on TravisCI"
    This reverts commit ebd88d3

--- check/pinger_test.go
+++ check/pinger_test.go
@@ -29,6 +29,9 @@ import (
 )
 
 func TestPinger_ValidLocalhost(t *testing.T) {
+	if runtime.GOOS != "darwin" {
+		t.Skip("Only runs without extra config on MacOS")
+	}
 	pinger, err := check.NewPinger("test1", check.IcmpNetUDP4, "127.0.0.1")
 	require.NoError(t, err)
 	require.NotNil(t, pinger)
@@ -42,6 +45,9 @@ func TestPinger_ValidLocalhost(t *testing.T) {
 }
 
 func TestPinger_Invalid127(t *testing.T) {
+	if runtime.GOOS != "darwin" {
+		t.Skip("Only runs without extra config on MacOS")
+	}
 	pinger, err := check.NewPinger("test1", check.IcmpNetUDP4, "127.0.0.2")
 	require.NoError(t, err)
 	require.NotNil(t, pinger)
@@ -53,6 +59,9 @@ func TestPinger_Invalid127(t *testing.T) {
 }
 
 func TestPinger_ValidLocalhostIPv6(t *testing.T) {
+	if runtime.GOOS != "darwin" {
+		t.Skip("Only runs without extra config on MacOS")
+	}
 	pinger, err := check.NewPinger("test1", check.IcmpNetUDP6, "::1")
 	require.NoError(t, err)
 	require.NotNil(t, pinger)
@@ -67,6 +76,10 @@ func TestPinger_ValidLocalhostIPv6(t *testing.T) {
 }
 
 func TestPinger_Concurrent(t *testing.T) {
+	if runtime.GOOS != "darwin" {
+		t.Skip("Only runs without extra config on MacOS")
+	}
+
 	if testing.Verbose() {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
